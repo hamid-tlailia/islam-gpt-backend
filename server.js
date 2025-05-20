a1de7db05aab5d857edc6d872925872ab97f624b
hamid-tlailia <tlhamid18@gmail.com>
2025-05-20 18:01:44 +0300

    Refactor server.js to use environment variable for PORT configuration

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.get("/health-check", (req, res) => {
   res.status(200).send("OK");
 });
 // Starting app server
-app.listen(5000, () =>
-  console.log("✅ Server running on http://localhost:5000")
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`✅ Server running on http://localhost:${PORT}`)
 );
+
